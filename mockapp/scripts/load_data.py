38b7de45513311a9f0f38c2a60ce10ac1321d8d7
ramirezg <gabriel.ramirez@gsa.gov>
2015-01-29 17:17:09 -0500

    fixing similar docs

--- mockapp/scripts/load_data.py
+++ mockapp/scripts/load_data.py
@@ -4,21 +4,23 @@ from django.utils.timezone import utc
 from mockapp.models import Tweet
 from mockapp.settings import BASE_DIR
 
-with open(BASE_DIR + '/mockapp/scripts/twitter_bank.csv', 'r') as csvfile:
-    header = csvfile.readline()
-    tweets = csv.reader(csvfile)
 
-    for row in tweets:
+def load_data():
+    with open(BASE_DIR + '/mockapp/scripts/twitter_bank.csv', 'r') as csvfile:
 
-        created_time = datetime.datetime.strptime(
-            row[6], "%a %b %d %H:%M:%S +0000 %Y")
-        created_time = created_time.replace(tzinfo=utc)
+        csvfile.readline()
+        tweets = csv.reader(csvfile)
 
-        t = Tweet(
-            postid=row[18][1:],
-            created_time=created_time,
-            from_user=row[15],
-            message=row[5],
-            retweets=row[11],
-            favorites=row[19])
-        t.save()
+        for row in tweets:
+            created_time = datetime.datetime.strptime(
+                row[6], "%a %b %d %H:%M:%S +0000 %Y")
+            created_time = created_time.replace(tzinfo=utc)
+
+            t = Tweet(
+                postid=row[18][1:],
+                created_time=created_time,
+                from_user=row[15],
+                message=row[5],
+                retweets=row[11],
+                favorites=row[19])
+            t.save()
